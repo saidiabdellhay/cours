6d013404a5655009605db2d17cd073d3465c3cd5
saidi abdellhay <asaidi.info@gmail.com>
2019-05-31 10:13:10 +0200

    promise

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -10,6 +10,7 @@ import { FooterComponent } from './footer/footer.component';
 import { MatSelectModule } from '@angular/material/select';
 import { MatSlideToggleModule } from '@angular/material/slide-toggle';
 import { ReactiveFormsModule } from '@angular/forms';
+import { MatProgressSpinnerModule } from '@angular/material/progress-spinner';
 
 import { MatFormFieldModule } from '@angular/material/form-field';
 import { MatInputModule } from '@angular/material/input';
@@ -68,6 +69,7 @@ import { LoginComponent } from './login/login.component';
     MatDialogModule,
     FormsModule,
     ReactiveFormsModule,
+    MatProgressSpinnerModule,
     AppRoutingModule
   ],
   providers: [
--- src/app/services/dish.service.ts
+++ src/app/services/dish.service.ts
@@ -1,6 +1,8 @@
 import { Injectable } from '@angular/core';
 import { Dish } from '../shared/dish';
 import { DISHES } from '../shared/dishes';
+import { of } from "rxjs";
+import { delay } from "rxjs/operators";
 
 @Injectable({
   providedIn: 'root'
@@ -9,16 +11,22 @@ export class DishService {
 
   constructor() { }
 
-  getDishes(): Dish[] {
-    return DISHES;
+  getDishes(): Promise<Dish[]> {
+    return new Promise(resolve => {
+      setTimeout(() => resolve(DISHES), 2000);
+    });
   }
 
-  getDish(id: string): Dish {
-    return DISHES.filter((dish) => (dish.id === id))[0];
+  getDish(id: string): Promise<Dish> {
+    return new Promise(resolve => {
+      setTimeout(() => resolve(DISHES.filter((dish) => (dish.id === id))[0]), 2000);
+    });
   }
 
-  getFeaturedDish(): Dish {
-    return DISHES.filter((dish) => dish.featured)[0];
+  getFeaturedDish(): Promise<Dish> {
+    return new Promise(resolve => {
+      setTimeout(() => resolve(DISHES.filter((dish) => dish.featured)[0]), 2000);
+    });
   }
   
 }
